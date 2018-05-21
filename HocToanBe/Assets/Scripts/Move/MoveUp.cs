4f76a4b7d29776bdb82422afad63ea4108b3e142
khoatyen2012 <33951988+khoatyen2012@users.noreply.github.com>
2018-05-21 16:29:59 +0700

    Lam do bay theo vecter huong
    vec

---
+++ HocToanBe/Assets/Scripts/Move/MoveUp.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveUp : MonoBehaviour {
+
+
+
+	public float moveSpeed;
+
+
+	public Vector3 startPosition;
+
+	public void setStartPosition()
+	{
+		this.transform.localPosition = startPosition;
+	}
+
+
+	public void setMove()
+	{
+		StartCoroutine(ieMoveUp());
+	}
+
+	IEnumerator ieMoveUp()
+	{
+		while (this.transform.localPosition.y < 0)
+		{
+			this.transform.localPosition += Vector3.up
+				* moveSpeed
+				* Time.deltaTime;
+			yield return 0;
+		}
+		this.transform.localPosition = new Vector3 (this.transform.localPosition.x, 0, this.transform.localPosition.z);
+	}
+
+	// Use this for initialization
+	void Start () {
+		startPosition = this.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- HocToanBe/Assets/Scripts/UI/InGame.cs
+++ HocToanBe/Assets/Scripts/UI/InGame.cs
@@ -9,9 +9,17 @@ public class InGame : MonoBehaviour {
     public Transform C3;
     public Transform O1;
     public Transform O2;
+	public MoveUp DD;
 
 	public int number = 1;
 	public Transform d1;
+	public Transform d2;
+	public Transform d3;
+	Transform dd;
+	Vector3 startPostionD;
+
+
+
 
 
     public void setData()
@@ -22,11 +30,12 @@ public class InGame : MonoBehaviour {
         O1.localScale = new Vector3(0, 0, 1);
         O2.localScale = new Vector3(0, 0, 1);
 
-        StartCoroutine(WaitTimeC1(1f,C1));
-        StartCoroutine(WaitTimeC1(2f, O1));
-        StartCoroutine(WaitTimeC1(3f, C2));
-        StartCoroutine(WaitTimeC1(4f, O2));
-        StartCoroutine(WaitTimeC1(5f, C3));
+        StartCoroutine(WaitTimeC1(0.3f,C1));
+        StartCoroutine(WaitTimeC1(1f, O1));
+        StartCoroutine(WaitTimeC1(1.5f, C2));
+        StartCoroutine(WaitTimeC1(2f, O2));
+        StartCoroutine(WaitTimeC1(3f, C3));
+		StartCoroutine(WaitTimeDD(4.2f));
     }
 
     IEnumerator WaitTimeC1(float time,Transform pC)
@@ -35,25 +44,53 @@ public class InGame : MonoBehaviour {
         pC.GetComponent<Zoom>().setZoom();
     }
 
+	IEnumerator WaitTimeDD(float time)
+	{
+		yield return new WaitForSeconds (time);
+		DD.setMove ();
+
+	}
+
 	// Use this for initialization
 	void Start () {
+
+
+	
 
 	}
 
 	public float speed;
 	void Update()
 	{
-		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+		if (Input.touchCount != 0 && Input.GetTouch(0).phase 
+			== TouchPhase.Began)
+		{
+			Vector3 positionTouch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			if (Mathf.Abs (positionTouch.x - d1.position.x) < (d1.GetComponent<Collider> ().bounds.size.x / 2) && Mathf.Abs (positionTouch.y - d1.position.y) < (d1.GetComponent<Collider> ().bounds.size.y / 2)) {
+				dd = d1;
+			} else if (Mathf.Abs (positionTouch.x - d2.position.x) < (d2.GetComponent<Collider> ().bounds.size.x / 2) && Mathf.Abs (positionTouch.y - d2.position.y) < (d2.GetComponent<Collider> ().bounds.size.y / 2)) {
+				dd = d2;
+			} else if (Mathf.Abs (positionTouch.x - d3.position.x) < (d3.GetComponent<Collider> ().bounds.size.x / 2) && Mathf.Abs (positionTouch.y - d3.position.y) < (d3.GetComponent<Collider> ().bounds.size.y / 2)) {
+				dd = d3;
+			} else {
+				dd = null;
+			}
+			startPostionD = dd.position;
+
+		}else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
 		{
 			// Get movement of the finger since last frame
 			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
 
 			// Move object across XY plane
-			d1.Translate(touchDeltaPosition.x * speed, touchDeltaPosition.y * speed, 0);
+			dd.Translate(touchDeltaPosition.x * speed, touchDeltaPosition.y * speed, 0);
 		}else if (Input.touchCount != 0 && Input.GetTouch(0).phase 
 			== TouchPhase.Ended)
 		{
 			number++;
+			dd.position = startPostionD;
+
+			dd = null;
 		
 		}
 	}
