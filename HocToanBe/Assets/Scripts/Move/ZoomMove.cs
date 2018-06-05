c3abb96568082beb7710fadf81cafe7dc496804b
khoatyen2012 <33951988+khoatyen2012@users.noreply.github.com>
2018-06-05 16:31:48 +0700

    Zoom nexxt

---
+++ HocToanBe/Assets/Scripts/Move/ZoomMove.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoomMove : MonoBehaviour {
+
+	public float startMax;
+
+	public float startMin;
+
+	public float distance;
+
+	public enum State
+	{
+		MIN,
+		MAX
+
+	}
+
+	public State curentState;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (curentState == State.MAX) {
+			if (this.transform.localScale.x < startMax)
+			{
+				this.transform.localScale = new Vector3(this.transform.localScale.x + distance, this.transform.localScale.y + distance,  this.transform.localScale.z);
+			}
+			else
+			{
+				curentState = State.MIN;
+			}
+		} else {
+			if (this.transform.localScale.x > startMin)
+			{
+				this.transform.localScale = new Vector3(this.transform.localScale.x - distance, this.transform.localScale.y - distance,  this.transform.localScale.z);
+			}
+			else
+			{
+				curentState = State.MAX;
+			}
+		}
+	}
+}
