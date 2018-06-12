f3ec7e40c2b652c6effbb486ec44749441b19394
Huy Vu <dinhthitampro@gmail.com>
2018-06-12 22:09:51 +0700

    Sounds

---
+++ HocToanBe/Assets/Scripts/Move/TapTap.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapTap : MonoBehaviour {
+
+    public Vector3 povisiEnd;
+    public Vector3 povisiStart;
+
+	// Use this for initialization
+	void Start () {
+        povisiStart = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
